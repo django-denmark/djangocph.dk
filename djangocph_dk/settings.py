4ddfd2c9b1211f0295d010b0f9e484fa03cf8598
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-16 23:06:12 +0100

    Setting up django-environ.

--- djangocph_dk/settings.py
+++ djangocph_dk/settings.py
@@ -1,33 +1,17 @@
-"""
-Django settings for djangocph_dk project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import environ
+
+env = environ.Env(DEBUG=(bool, False),)
+environ.Env.read_env()
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+SECRET_KEY = env('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+DEBUG = env('DEBUG')
+TEMPLATE_DEBUG = DEBUG
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(y8yzgx+&61v%=amr=3=q@p9o8_pz7z3zef0w3nu6p)^!0tw+l'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -52,19 +36,9 @@ ROOT_URLCONF = 'djangocph_dk.urls'
 
 WSGI_APPLICATION = 'djangocph_dk.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db()
 }
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -76,8 +50,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'static_src'),
+)
+
+TEMPLATE_DIRS = {
+    os.path.join(os.path.dirname(__file__), 'templates')
+}
