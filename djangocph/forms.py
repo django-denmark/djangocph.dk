76f42740762fa821a9436adba6c98ff61e300816
Emil Kjer <emil@opbeat.com>
2015-01-16 23:52:34 +0100

    contact form send mail aka log-it TM

---
+++ djangocph/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ContactForm(forms.Form):
+    name = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea)
+
+    def send_email(self):
+        msg = "Name: {0}, Message: {1}".format(self.name, self.message)
+        logger.info(msg)
+        return True
--- djangocph/views.py
+++ djangocph/views.py
@@ -1,5 +1,17 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
+from . import forms
 
 class Website(TemplateView):
     template_name = 'website.html'
+
+
+class ContactView(FormView):
+    template_name = 'contact.html'
+    form_class = forms.ContactForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+        form.send_email()
+        return super(ContactView, self).form_valid(form)
