2ee3ff0dfa7b90d5904efd3b6131567888ad3742
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-17 19:30:33 +0100

    Order?

--- djangocph_dk/wsgi.py
+++ djangocph_dk/wsgi.py
@@ -1,6 +1,8 @@
 import os
-from whitenoise.django import DjangoWhiteNoise
-from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangocph_dk.settings")
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
