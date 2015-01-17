9a88ee415600498400528c37384245b29ba198b0
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-17 20:35:50 +0100

    Heroku doesn't work with naked domains.

--- djangocph_dk/wsgi.py
+++ djangocph_dk/wsgi.py
@@ -3,6 +3,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangocph_dk.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
