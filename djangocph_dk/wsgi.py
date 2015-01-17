9729995e17330821459a4a6f2436d0fd4384b724
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-17 19:21:28 +0100

    Now it _should_ work...

--- djangocph_dk/wsgi.py
+++ djangocph_dk/wsgi.py
@@ -1,4 +1,6 @@
 import os
 from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangocph_dk.settings")
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
